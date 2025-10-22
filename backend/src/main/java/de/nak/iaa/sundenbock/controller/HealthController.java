d756db233443a6999b7b18182d2c0cc6ad2f695d
Daria Stolarczyk <daria.stolarczyk@outlook.de>
2025-10-22 19:09:52 +0200

    setup(backend): cors settings dies das

--- backend/src/main/java/de/nak/iaa/sundenbock/controller/HealthController.java
+++ backend/src/main/java/de/nak/iaa/sundenbock/controller/HealthController.java
@@ -1,4 +1,4 @@
-package de.nak.iaa.sundenbock.web;
+package de.nak.iaa.sundenbock.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
