04a929a87cd990c77fd8db23abb416c67294276b
Daria Stolarczyk <daria.stolarczyk@outlook.de>
2025-10-22 18:05:05 +0200

    setup(backend): stupid backend with test api healthcheck

---
+++ backend/src/main/java/de/nak/iaa/sundenbock/SundenbockApplication.java
@@ -0,0 +1,11 @@
+package de.nak.iaa.sundenbock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SundenbockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SundenbockApplication.class, args);
+    }
+}
