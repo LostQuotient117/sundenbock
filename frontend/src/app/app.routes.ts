23c9a4bee1819be7cbf5aa498713e2446b137ea2
Nina Pick <nina.pick@nordakademie.de>
2025-10-29 18:06:01 +0100

    create pretty projects fronti

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Health } from './features/health/health';
 import { Tickets } from './features/tickets/tickets';
+import { Projects } from './features/projects/projects';
 
 export const routes: Routes = [
      { path: 'health', component: Health },
      { path: 'tickets', component: Tickets },
+     { path: 'projects', component: Projects },
   { path: '', pathMatch: 'full', redirectTo: 'health' }
 ];
