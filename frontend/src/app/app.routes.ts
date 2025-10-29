657d858172f02afb391dee1e2e9e1c4af364585e
Nina Pick <nina.pick@nordakademie.de>
2025-10-29 16:34:23 +0100

    easy display of tickets on /tickets url

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Health } from './features/health/health';
+import { Tickets } from './features/tickets/tickets';
 
 export const routes: Routes = [
      { path: 'health', component: Health },
+     { path: 'tickets', component: Tickets },
   { path: '', pathMatch: 'full', redirectTo: 'health' }
 ];
--- frontend/src/app/features/tickets/tickets.ts
+++ frontend/src/app/features/tickets/tickets.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TicketsService } from '../../core/services/tickets.service';
 
 @Component({
   selector: 'app-tickets',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tickets.html',
   styleUrl: './tickets.css'
 })
 export class Tickets {
+   tickets = signal<any[]>([]);
+  loading = signal(false);
 
+  constructor(private ticketsService: TicketsService) {
+    this.loadTickets();
+  }
+
+  loadTickets() {
+    this.loading.set(true);
+    this.ticketsService.getTickets$().subscribe({
+      next: (data) => this.tickets.set(data),
+      error: () => this.tickets.set([]),
+      complete: () => this.loading.set(false)
+    });
+  }
 }
