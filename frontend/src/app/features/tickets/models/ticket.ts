68a5a79c2c78f1a0c824d58d49520cff1998b93b
Nina Pick <nina.pick@nordakademie.de>
2025-11-06 18:14:29 +0100

    changing author property to responsiblePerson (like backend entity)

--- frontend/src/app/features/tickets/models/ticket.ts
+++ frontend/src/app/features/tickets/models/ticket.ts
@@ -6,7 +6,7 @@ export interface Ticket {
   title: string;
   description?: string;
   status: 'CREATED' | 'REOPENED' | 'IN_PROGRESS' | 'RESOLVED' | 'REJECTED' | 'CLOSED';
-  author: User;
+  //author: User;
   responsiblePerson?: User;
   project?: Project;
   createdOn: Date;
