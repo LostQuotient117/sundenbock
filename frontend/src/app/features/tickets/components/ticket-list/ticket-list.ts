9248ed1badf23f698f6242cff117e53587fbcaad
Nina Pick <nina.pick@nordakademie.de>
2025-11-08 02:28:04 +0100

    clean up

--- frontend/src/app/features/tickets/components/ticket-list/ticket-list.ts
+++ frontend/src/app/features/tickets/components/ticket-list/ticket-list.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HydratedTicket } from '../../../../shared/models/types';
 
