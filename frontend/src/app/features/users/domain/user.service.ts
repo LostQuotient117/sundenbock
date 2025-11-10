fea3c773c760e4b8e6e263df81ef56a8812436ab
Nina Pick <nina.pick@nordakademie.de>
2025-11-10 15:26:13 +0100

    user dto durchreichen über user.service

--- frontend/src/app/features/users/domain/user.service.ts
+++ frontend/src/app/features/users/domain/user.service.ts
@@ -3,12 +3,17 @@ import { shareReplay } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { UsersClient } from '../data/users.client';
 import { User } from './user.model';
+import { UserDetailDto, UserDto } from '../data/user.dto';
 
 @Injectable({ providedIn: 'root' })
 export class UsersService {
   private api = inject(UsersClient);
 
-  listAll(): Observable<User[]> {
+  listAll(): Observable<UserDto[]> {
     return this.api.listAll().pipe(shareReplay(1));
+  }
+
+  details(username: string): Observable<UserDetailDto> {
+    return this.api.getDetails(username);
   }
 }
