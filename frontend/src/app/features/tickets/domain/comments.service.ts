af8b966b9cdb50cf669aca4b9e4efa4c57662369
Nina Pick <nina.pick@nordakademie.de>
2025-11-10 18:10:00 +0100

    CommentsService angelegt; lädt Kommentare pro Ticket

---
+++ frontend/src/app/features/tickets/domain/comments.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, inject } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Page, PageQuery } from '@shared/models/paging';
+import { CommentsClient } from '@features/tickets/data/comments.client';
+import { CommentDto } from '@features/tickets/data/comment.dto';
+import { Comment } from './comment.model';
+import { mapPage } from '@shared/utils/mapping.base';
+import { mapComment } from './comment.mapper';
+
+@Injectable({ providedIn: 'root' })
+export class CommentsService {
+  private api = inject(CommentsClient);
+
+  listByTicket(ticketId: number | string, q?: PageQuery<CommentDto>): Observable<Page<Comment>> {
+    return this.api.listByTicket(ticketId, q).pipe(map((p) => mapPage(p, mapComment)));
+  }
+}
