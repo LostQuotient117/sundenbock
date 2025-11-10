d644e67f4b2954c8f5a15e3df4249d4539b7b5a4
Nina Pick <nina.pick@nordakademie.de>
2025-11-10 15:23:42 +0100

    umstellung user.client auf erzeugte user dtos

--- frontend/src/app/features/users/data/users.client.ts
+++ frontend/src/app/features/users/data/users.client.ts
@@ -4,15 +4,21 @@ import { ApiService } from '@core/http/api.service';
 import { HttpContext } from '@angular/common/http';
 import { SUPPRESS_403_REDIRECT } from '@core/http/http-context';
 import { User } from '@features/users/domain/user.model';
+import { UserDetailDto, UserDto } from './user.dto';
 
 @Injectable({ providedIn: 'root' })
 export class UsersClient {
   private api = inject(ApiService);
 
   // Lädt alle User
-  listAll(): Observable<User[]> {
+  listAll(): Observable<UserDto[]> {
     const ctx = new HttpContext().set(SUPPRESS_403_REDIRECT, true);
-    return this.api.get<User[]>('/users', undefined, { context: ctx });
-    // Erwartet: GET /api/v1/users -> User[]
+    return this.api.get<UserDto[]>('/users', undefined, { context: ctx });
+  }
+
+  // Einzelne User mit Rollen + Berechtigungen
+  getDetails(username: string): Observable<UserDetailDto> {
+    const ctx = new HttpContext().set(SUPPRESS_403_REDIRECT, true);
+    return this.api.get<UserDetailDto>(`/users/${encodeURIComponent(username)}/details`, undefined, { context: ctx });
   }
 }
